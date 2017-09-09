daeecbbb30cd0945d17f9ebec4fb2df96376c9f8
Elizabeth Yeoh-Wang <elizabethyeoh-wang@Elizabeth.local>
2017-09-09 14:59:15 -0400

    map

--- FastMed/index.ios.js
+++ FastMed/index.ios.js
@@ -12,32 +12,43 @@ import {
   Text,
   View
 } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class FastMed extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      latitude: null,
-      longitude: null,
+      latitude: 27.78825,
+      longitude: -132.4324,
       error: null,
     };
   }
 
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
+  componentWillMount() {
+      navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            error: null,
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
         });
       },
-      (error) => this.setState({ error: error.message }),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-      );
+      (error) => alert(error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+    
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      this.setState({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      });
+      //this.forceUpdate();
+    });
   }
 
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchId);
+  }
 
 /*<View style={styles.container}>
         <Text style={styles.welcome}>
@@ -52,20 +63,38 @@ export default class FastMed extends Component {
         </Text>
       </View>
   
-    React.createElement(Text, {style: styles.instructions}, "Get the help you need.")
 
-      */
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
+  <Text style={styles.welcome}>
           Latitude: {this.state.latitude}
         </Text>
         <Text style={styles.welcome}>
           Longitude: {this.state.longitude}
           {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
         </Text>
+
+    React.createElement(Text, {style: styles.instructions}, "Get the help you need.")
+37.78825
+-122.4324
+      */
+
+  render() {
+    return (
+      <View style={styles.map}>
+        <MapView
+          initialRegion={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          region={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          style={styles.map}
+        />
       </View>
     );
   }
@@ -89,6 +118,9 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     marginTop: 65,
   },
+  map: {
+    flex: 1,
+  }
 });
 
 AppRegistry.registerComponent('FastMed', () => FastMed);
